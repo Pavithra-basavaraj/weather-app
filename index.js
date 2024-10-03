98f0af67d7bc251b8aeeea8619db54a0e175c055
Pavithra-basavaraj <pavithrasbasavaraj@gmail.com>
2024-10-03 13:11:46 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+let gameNumber=7;
+let userNumber=prompt("Guesses the game number");
+
+    while(userNumber!=gameNumber)
+    {
+        userNumber=prompt("Guess again");
+
+
+    }
+    console.log("you won the match");
